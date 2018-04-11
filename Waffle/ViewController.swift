9d59e60e892efe57f98be72b2ca910b9d9d2f80f
astghikhakopian <astghik.hakopian@gmail.com>
2018-04-11 23:22:33 +0400

    Initial Commit

---
+++ Waffle/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Waffle
+//
+//  Created by Astghik Hakopian on 4/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
