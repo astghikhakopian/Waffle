319c64c2cc2edc0602c6727077435e719f55aa20
Sierra os <sierraos@macbook-air-sierra.local>
2018-04-30 13:54:41 +0400

    contacts' avatans are already round

--- Waffle/Contacts/ContactsViewController.swift
+++ Waffle/Contacts/ContactsViewController.swift
@@ -68,10 +68,20 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
             do {
                 let imageData = try Data(contentsOf: theProfileImageUrl as URL)
                 cell.imageVIew.image = UIImage(data: imageData)
+                cell.imageVIew.layer.borderWidth=1.0
+                cell.imageVIew.layer.borderColor = UIColor.white.cgColor
+                cell.imageVIew.layer.masksToBounds = false
+                cell.imageVIew.layer.cornerRadius = cell.imageVIew.frame.size.height/2
+                cell.imageVIew.clipsToBounds = true
             } catch {
                 print("Unable to load data: \(error)")
             }
         } else {
+            cell.imageVIew.layer.borderWidth=1.0
+            cell.imageVIew.layer.masksToBounds = false
+            cell.imageVIew.layer.cornerRadius = cell.imageVIew.frame.size.height/2
+            cell.imageVIew.layer.borderColor = UIColor.white.cgColor
+            cell.imageVIew.clipsToBounds = true
             cell.imageVIew.image = UIImage(named: "defaultProfile")
         }
         let tap = TapRecognizer(target: self, action: #selector(self.handleTap(gestureRecognizer:)))
