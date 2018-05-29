1bf9d2483f566019accb74d8b6fd82a1eb4b9eaa
astghikhakopian <astghik.hakopian@gmail.com>
2018-05-30 02:32:04 +0400

    refactoring Login

--- Waffle/Login/TutorialViewController.swift
+++ Waffle/Login/TutorialViewController.swift
@@ -9,42 +9,22 @@ import UIKit
 
 class TutorialViewController: UIViewController {
 
+    // MARK: - Properties
+    
     @IBOutlet var tutorialView: UIView!
     
+    
+    // MARK: - Actions
     
     @IBAction func tryButtonAction(_ sender: Any) {
         let viewController = self.storyboard?.instantiateViewController(withIdentifier: "tutorialVC2")
         UIApplication.shared.keyWindow?.rootViewController = viewController
     }
-    @IBAction func skipButtonAction(_ sender: Any) {
-        moveToVC(withIdentifier: "loggedInVC")
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-    private func moveToVC(withIdentifier identifier: String) {
-        if let viewController = self.storyboard?.instantiateViewController(withIdentifier: identifier) {
+    @IBAction func skipButtonAction(_ sender: Any) {
+        if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "loggedInVC") {
             UIApplication.shared.keyWindow?.rootViewController = viewController
             self.dismiss(animated: true, completion: nil)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
